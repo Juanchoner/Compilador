55d9f5d7ac241291a8911ea3ad836fbac0cf8d5a
imangolf1313 <osvaldo.lopez.is@unipolidgo.edu.mx>
2022-03-17 07:41:33 -0600

    reglas de producción

--- parser.py
+++ parser.py
@@ -12,4 +12,11 @@ def p_expression_minus(p):
 def p_expression_term(p):
     'expression: expression term'
     p[0] = p[1]
-    
+
+def p_factor_expr(p):
+     'factor : LPAREN expression RPAREN'
+     p[0] = p[2]
+ 
+ # Error rule for syntax errors
+def p_error(p):
+    print("Syntax error in input!")
